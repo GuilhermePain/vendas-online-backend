44a50de0d479428eeba9081a3f4651f5b44e3a70
GuilhermePain <guizaorocha543@gmail.com>
2025-03-14 11:21:06 -0300

    feat: creating address, city and state entities and creating service and state controller

--- src/migration/1741874318149-create_table_address.ts
+++ src/migration/1741874318149-create_table_address.ts
@@ -1,6 +1,6 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
 
-export class CreateTableAddress1741878289045 implements MigrationInterface {
+export class CreateTableAddress1741874318149 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         queryRunner.query(`
